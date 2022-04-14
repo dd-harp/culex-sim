3156edb5a45a866593c1a27e47fc29e90e6b8f80
slwu89 <10673535+slwu89@users.noreply.github.com>
2022-04-13 17:03:25 -0700

    tests for stage advancement

--- tests/testthat/test-culex_inf.R
+++ tests/testthat/test-culex_inf.R
@@ -22,15 +22,77 @@ test_that("stochastic model object is created correctly", {
   expect_true(nrow(get_AI_culex_infection_stochastic(mod)) == 1L)
   expect_true(ncol(get_AI_culex_infection_stochastic(mod)) == p)
   
+})
+
+
+test_that("stochastic model object has correct stage advancement", {
+  parameters <- culex_parameters()
+  p <- 2
+  tau_E <- c(5,6,7,6,5)
+  tau_L <- c(2,3,2,3,3)
+  tau_P <- c(4,5,5,5,4)
+  tau_EIP <- c(6,5,4,6,7)
+  dt <- 0.5
+  psi <- diag(2)
+  n_species <- 2
+  
+  # check AE advances through EIP properly
+  mod <- create_culex_infection_stochastic(p = p, tau_E = tau_E, tau_L = tau_L, tau_P = tau_P, tau_EIP = tau_EIP, dt = dt, psi = psi, n_species = n_species)
+  
   newE <- matrix(0, nrow = max(tau_EIP), ncol = p)
   newE[max(tau_EIP), ] <- 1e3
   set_AE_culex_infection_stochastic(mod, newE)
-  # testAE <- newE*0
-  # testAE[max(tau_EIP)-1, ] <- 10
   
-  get_AE_culex_infection_stochastic(mod)
   step_culex_infection_stochastic(mod = mod, parameters = parameters)
-  get_AE_culex_infection_stochastic(mod)
+  outE <- get_AE_culex_infection_stochastic(mod)
   
-  expect_true()
+  expect_true(all(outE[max(tau_EIP) -1 ,] > 0))
+  expect_true(sum(outE) <= sum(newE))
+  expect_true(all(outE[-(max(tau_EIP) -1), ] == 0))
+  
+  # check AE goes to AI at end of their incubation period
+  mod <- create_culex_infection_stochastic(p = p, tau_E = tau_E, tau_L = tau_L, tau_P = tau_P, tau_EIP = tau_EIP, dt = dt, psi = psi, n_species = n_species)
+  
+  newE <- newE*0
+  newE[1, ] <- 1e3
+  set_AE_culex_infection_stochastic(mod, newE)
+  
+  step_culex_infection_stochastic(mod = mod, parameters = parameters)
+  
+  outE <- get_AE_culex_infection_stochastic(mod)
+  outI <- get_AI_culex_infection_stochastic(mod)
+  
+  expect_true(sum(outE) == 0)
+  expect_true(sum(outI) <= sum(newE))
+  
+  # check new infecteds go to the right spot in EIP
+  mod <- create_culex_infection_stochastic(p = p, tau_E = tau_E, tau_L = tau_L, tau_P = tau_P, tau_EIP = tau_EIP, dt = dt, psi = psi, n_species = n_species)
+  
+  set_AS_culex_infection_stochastic(mod, c(1e3, 1e3))
+  set_f_culex_infection_stochastic(mod, c(5, 5))
+  set_q_culex_infection_stochastic(mod, matrix(0.5, 2, 2))
+  set_kappa_culex_infection_stochastic(mod, matrix(0.5, 2, 2))
+  
+  step_culex_infection_stochastic(mod = mod, parameters = parameters)
+  outAS <- get_AS_culex_infection_stochastic(mod)
+  outAE <- get_AE_culex_infection_stochastic(mod)
+  
+  expect_true(sum(outAS) < 2e3)
+  expect_true(sum(outAE[-tau_EIP[1], ]) == 0)
+  expect_true(sum(outAE) > 0)
+  
+  # check that infected pupae go to I adults
+  mod <- create_culex_infection_stochastic(p = p, tau_E = tau_E, tau_L = tau_L, tau_P = tau_P, tau_EIP = tau_EIP, dt = dt, psi = psi, n_species = n_species)
+  
+  newP <- matrix(0, nrow = max(tau_P), ncol = p)
+  newP[1, ] <- 1e3
+  set_PI_culex_infection_stochastic(mod, newP)
+  set_AI_culex_infection_stochastic(mod, c(0, 0))
+  set_AS_culex_infection_stochastic(mod, c(0, 0))
+  
+  step_culex_infection_stochastic(mod = mod, parameters = parameters)
+  
+  expect_true(sum(get_PI_culex_infection_stochastic(mod)) == 0)
+  expect_true(sum(get_AI_culex_infection_stochastic(mod)) <= sum(newP))
+  expect_true(sum(get_AS_culex_infection_stochastic(mod)) == 0)
 })
